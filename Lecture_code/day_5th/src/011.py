41e98b5fa988b38117c69ff37229062eae222ce2
DDAEGEEE <taykee58@gmail.com>
2022-08-04 20:57:53 +0900

    6th day _ 3

---
+++ Lecture_code/day_5th/src/011.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import utils
+
+IMG_PATH = "C:/Users/LG/Documents/GitHub/ict_2022_2/Lecture_code/day_5th/images"
+
+
+if __name__ == "__main__":
+    img = np.zeros((512, 512, 3), np.uint8)
+    imag = cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
+    utils.show_img(img)
+
+    img = cv2.rectangle(img, (384, 0), (510, 128), (0, 255, 0), 3)
+    utils.show_img(img)
+
+    img = cv2.circle(img, (447, 63), 63, (0, 0, 255), -1)
+    utils.show_img(img)
+
+    img = cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, 255, -1)
+    utils.show_img(img)
+
---
+++ Lecture_code/day_5th/src/012.py
@@ -0,0 +1,28 @@
+import os
+
+import cv2
+import numpy as np
+
+IMG_PATH = "C:/Users/LG/Documents/GitHub/ict_2022_2/Lecture_code/day_5th/images"
+
+
+def nothing(x):
+    pass
+
+
+if __name__ == "__main__":
+    img = cv2.imread(os.path.join(IMG_PATH, "lena.jpeg"))
+
+    for k in range(20):
+        if k == 0:
+            continue
+
+        cv2.namedWindow("image k: {k}")
+
+        kernel = np.ones((k, k), np.float32) / (k * 2)
+        dst = cv2.filter2D(img, -1, kernel)
+
+        cv2.imshow(f"K: {k}", dst)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
